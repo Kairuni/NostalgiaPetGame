68c489ce433fc52e05071766d8e9f793a313b390
Capt_Spencer <Capt-Spencer@Hotmail.com>
2018-05-22 17:13:27 -0700

    Debugging onPause() and onResume().  Added call to super.  Some errors with final fields.

--- app/src/main/java/games/wantz/spencer/nostalgiapetgame/GameActivity.java
+++ app/src/main/java/games/wantz/spencer/nostalgiapetgame/GameActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+
+import games.wantz.spencer.nostalgiapetgame.drawing.GameView;
 
 /**
  * The activity used for actually playing the game.
@@ -13,17 +16,12 @@ import android.view.WindowManager;
  * @version 0.1, 11 May 2018
  */
 public class GameActivity extends AppCompatActivity {
-    /**
-     * Value of the JSON return for UID.
-     */
+
+    /** Value of the JSON return for UID. */
     public final static String UID = "UID";
-    /**
-     * Value of the JSON return for Breed.
-     */
+    /** Value of the JSON return for Breed. */
     public final static String BREED = "Breed";
-    /**
-     * Value of the JSON return for MaxHealth.
-     */
+    /** Value of the JSON return for MaxHealth. */
     public final static String MAX_HEALTH = "MaxHealth";
     /** Value of the JSON return for Health. */
     public final static String HEALTH = "Health";
@@ -58,22 +56,47 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
 
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Intent intent = getIntent();
 
         Log.d("Game", "Creating monster.");
-        /*    public Monster(String mUID, int mBreed, float mMaxHealth, float mHealth, float mMaxStamina,
-                   float mStamina, float mMaxHunger, float mHunger, float mMaxBladder, float mBladder) {*/
+        /* public Monster(String mUID, int mBreed, float mMaxHealth, float mHealth, float mMaxStamina,
+                float mStamina, float mMaxHunger, float mHunger, float mMaxBladder, float mBladder) {*/
         mMonster = new Monster(intent.getStringExtra(UID), intent.getIntExtra(BREED, 0),
-                intent.getFloatExtra(MAX_HEALTH, 0.0f), intent.getFloatExtra(HEALTH, 0.0f),
-                intent.getFloatExtra(MAX_STAMINA, 0.0f), intent.getFloatExtra(STAMINA, 0.0f),
-                intent.getFloatExtra(MAX_HUNGER, 0.0f), intent.getFloatExtra(HUNGER, 0.0f),
-                intent.getFloatExtra(MAX_BLADDER, 0.0f), intent.getFloatExtra(BLADDER, 0.0f));
+                intent.getFloatExtra(MAX_HEALTH, 0.0f),
+                intent.getFloatExtra(HEALTH, 0.0f),
+                intent.getFloatExtra(MAX_STAMINA, 0.0f),
+                intent.getFloatExtra(STAMINA, 0.0f),
+                intent.getFloatExtra(MAX_HUNGER, 0.0f),
+                intent.getFloatExtra(HUNGER, 0.0f),
+                intent.getFloatExtra(MAX_BLADDER, 0.0f),
+                intent.getFloatExtra(BLADDER, 0.0f));
         Log.d("Game", "Created monster.");
-
+        //View v = getLayoutInflater().inflate(R.layout.activity_game, null, false);
+        View gvp = findViewById(R.id.button_shower);
+        GameView gvr = findViewById(R.id.game_play_view_zzzzz);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        View vp = getLayoutInflater().inflate(R.layout.activity_game, null, false);
+        GameView gvp = vp.findViewById(R.id.game_play_view_zzzzz);
+        if (gvp != null) {
+            gvp.gameViewPause();
+        }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View vr = getLayoutInflater().inflate(R.layout.activity_game, null, false);
+        GameView gvr = vr.findViewById(R.id.game_play_view_zzzzz);
+        if (gvr != null) {
+            gvr.gameViewResume();
+        }
+    }
 }
--- app/src/main/java/games/wantz/spencer/nostalgiapetgame/drawing/GameView.java
+++ app/src/main/java/games/wantz/spencer/nostalgiapetgame/drawing/GameView.java
@@ -34,7 +34,7 @@ public class GameView extends SurfaceView {
     /**
      * Our game thread.
      */
-    private final GameThread mGameThread;
+    private GameThread mGameThread;
     /** The player's monster. */
     private Monster mMonster;
     /** The value to scale our sprite sheets by to fit the device screen. */
@@ -52,7 +52,7 @@ public class GameView extends SurfaceView {
      * @param context The context in which this View is made.
      */
     public GameView(Context context) {
-        super(context, null);
+        this(context, null);
 
         // This is a bit convoluted, but it retrieves the device width/height.
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -69,8 +69,10 @@ public class GameView extends SurfaceView {
         mMonsterFrame = 29;
 
         // Make our spritesheets.
-        mBackground = new SpriteSheet(BitmapFactory.decodeResource(getResources(), R.drawable.main_background), 160, 90, mScalar);
-        mUnits = new SpriteSheet(BitmapFactory.decodeResource(getResources(), R.drawable.pets_and_icons), 32, 32, mScalar);
+        mBackground = new SpriteSheet(BitmapFactory.decodeResource(getResources(),
+                R.drawable.main_background), 160, 90, mScalar);
+        mUnits = new SpriteSheet(BitmapFactory.decodeResource(getResources(),
+                R.drawable.pets_and_icons), 32, 32, mScalar);
 
         // Makes our thread.
         mGameThread = new GameThread(this);
@@ -97,6 +99,7 @@ public class GameView extends SurfaceView {
                         mGameThread.join();
                         retry = false;
                     } catch (InterruptedException e) {
+
                     }
                 }
             }
@@ -109,7 +112,21 @@ public class GameView extends SurfaceView {
 
     /** Necessary constructor, calls the other constructor. */
     public GameView(Context context, AttributeSet set) {
-        this(context);
+        super(context, set);
+    }
+
+
+
+    public void gameViewPause() {
+        mGameThread.setActive(false);
+        mGameThread = null;
+    }
+
+    public void gameViewResume() {
+        if (mGameThread == null) {
+            mGameThread = new GameThread(this);
+            mGameThread.setActive(true);
+        }
     }
 
     /**
